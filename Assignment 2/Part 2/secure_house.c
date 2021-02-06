04c3d4b981a6a0af23eb3065b4266592493b0a18
CharlieHex <charliehex@gmail.com>
2021-02-05 21:44:16 -0700

    Cleaned comments and organized a bit

--- Assignment 2/Part 2/secure_house.c
+++ Assignment 2/Part 2/secure_house.c
@@ -15,7 +15,10 @@
 
 using namespace std; 
 
+// Server owner keys
 const char *keys[10];
+
+
 const char *USER_EVENT_1[1];
 const char *USER_EVENT_2[1];
 const char *USER_EVENT_FINAL[1];
@@ -25,8 +28,6 @@ const char *USER_KEY[1];
 
 int main( int argc, char *argv[] )  {
 
-//Loop Main until EOF. Possible while(!EOF)
-
 // Handle Initial Command Line Input
     if( argc == 2 ) {
       printf("No key supplied");
@@ -34,38 +35,39 @@ int main( int argc, char *argv[] )  {
     // Create an array for keys
    	else if( argc > 2 ) {
         
+        // Assign server owner for later re-keying function
         char *serverOwner = argv[1];
 
-        int count = 0;
-        
         // Copy keys into memory
+        int count = 0;
+
         for(int i = 2; i < argc; i++){
 			keys[count] = argv[i];
             count++;
        	}
     }
-
    	else {
       	printf("At least one key expected.\n");
    	}
-
-    //Loop until EOF. Possible while(!EOF)
     
+    //Local array for command line input
     char USER_INPUT[50] = " ";
 
     int len = strlen(USER_INPUT);
-
     
-    
+    //Loop until EOF.-----------------------------------------------
     while (len != 0)
     {
         cout << "Please enter your input commands:\n";
+        // Get the user from the input
         cin.getline(USER_INPUT, 50);
-        //cout << "\nTESTING ECHO: " << USER_INPUT << "\n";
         len = strlen(USER_INPUT);
         //cout << "User input is " << len << " characters long.\n";
         if(len == 1)
             len = 0;
+
+        // Convert USER_INPUT into an event, user and key
+
     }
     
     cout << "Goodbye.";
@@ -74,8 +76,5 @@ int main( int argc, char *argv[] )  {
 
     // Convert USER_INPUT into an event, user and key
     // Read user input to end of line
-
-    /*for (int ndx = 0; ndx <= UCHAR_MAX; ndx++)
-        if (isspace(ndx)) printf(USER_INPUT[ndx], ndx);*/
 
 }
